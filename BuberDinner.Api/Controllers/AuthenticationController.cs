1be978e85bd5cd148f58a788c0867eb56a50ecb3
Brian MacPherson <brimac1634@gmail.com>
2023-05-24 23:25:46 +0800

    intial commit

---
+++ BuberDinner.Api/Controllers/AuthenticationController.cs
@@ -0,0 +1,53 @@
+using BuberDinner.Application.Services.Authentication;
+using BuberDinner.Contracts.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuberDinner.Api.Controllers;
+
+[ApiController]
+[Route("auth")]
+public class AuthenticationController : ControllerBase
+{
+    private IAuthenticationService _authenticationService;
+
+    public AuthenticationController(IAuthenticationService authenticationService)
+    {
+        _authenticationService = authenticationService;
+    }
+    
+    [HttpPost("register")]
+    public IActionResult Register(RegisterRequest request)
+    {
+        var authResult = _authenticationService.Register(
+            request.FirstName,
+            request.LastName,
+            request.Email,
+            request.Password);
+
+        var response = new AuthenticationResponse(
+            authResult.Id,
+            authResult.FirstName,
+            authResult.LastName,
+            authResult.Email,
+            authResult.Token);
+
+        return Ok(response);
+    }
+
+    [HttpPost("login")]
+    public IActionResult Login(LoginRequest request)
+    {
+        var authResult = _authenticationService.Login(
+            request.Email,
+            request.Password);
+        
+        var response = new AuthenticationResponse(
+            authResult.Id,
+            authResult.FirstName,
+            authResult.LastName,
+            authResult.Email,
+            authResult.Token);
+
+        return Ok(response);
+    }
+}
---
+++ BuberDinner.Api/Program.cs
@@ -0,0 +1,20 @@
+using BuberDinner.Application;
+using BuberDinner.Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+{
+    builder
+        .Services
+        .AddApplication()
+        .AddInfrastructure();
+
+    builder.Services.AddControllers();
+}
+
+var app = builder.Build();
+{
+    app.UseHttpsRedirection();
+    app.MapControllers();
+
+    app.Run();
+}
