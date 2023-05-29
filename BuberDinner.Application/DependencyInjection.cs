2fb93a65039db4b174b015ff068fb5b4a8f225e1
Brian MacPherson <brimac1634@gmail.com>
2023-05-29 21:31:37 +0800

    Resolved mediatr library conflict

--- BuberDinner.Application/DependencyInjection.cs
+++ BuberDinner.Application/DependencyInjection.cs
@@ -7,7 +7,7 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
-        services.AddMediatR(typeof(DependencyInjection).Assembly);
+        services.AddMediatR(config => config.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly));
         
         return services;
     }
